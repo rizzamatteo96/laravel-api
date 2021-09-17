392b6ab9eb8bf5f46d912d964e49a3b14e59cd95
rizzamatteo96 <77894068+rizzamatteo96@users.noreply.github.com>
2021-09-17 15:19:58 +0200

    add header and main components to app vue

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container mt-5">
+    <div class="row row-cols-3 g-4">
+      <div class="col mt-4" v-for="post in posts" :key="post.id"> 
+        <div class="card text-dark bg-light">
+          <div class="card-body">
+            <h3 class="card-title">{{post.title}}</h3>
+            <p class="card-text">{{post.description}}</p>
+            <p class="card-text"><small class="text-muted">{{post.created_at}}</small></p>
+            <button type="button" class="btn btn-outline-dark">Leggi articolo</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Main',
+  data(){
+    return {
+      urlApi: 'http://127.0.0.1:8000/api/posts' ,
+      posts: []
+    }
+  },
+  created(){
+    this.getPosts();
+  },
+  methods: {
+    getPosts(){
+      axios.get(this.urlApi)
+            .then(response => {
+              // console.log(response.data.results);
+              this.posts = response.data.results;
+            });
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/admin.js', 'public/js')
     .js('resources/js/front.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .options({
-        processCssUrls: false
-    });
+    .sass('resources/sass/app.scss', 'public/css');
